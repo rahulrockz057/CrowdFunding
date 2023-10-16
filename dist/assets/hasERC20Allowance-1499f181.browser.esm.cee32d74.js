26f58290dbcb153421b8a1320ddeae72dea56649
rahulrockz057 <85492371+rahulrockz057@users.noreply.github.com>
2023-10-17 03:38:56 +0530

    Add files via upload

---
+++ dist/assets/hasERC20Allowance-1499f181.browser.esm.cee32d74.js
@@ -0,0 +1,1 @@
+import{C as d,ag as i}from"./index.815e7d61.js";async function c(a,e,s){const n=a.getProvider(),r=new d(n,e,i,{},a.storage),t=await a.getSignerAddress(),o=a.address;return(await r.read("allowance",[t,o])).gte(s)}export{c as h};
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Route, Routes }from 'react-router-dom';
+import Sidebar from './components/.Sidebar'; // Replace './Sidebar' with the actual path to your Sidebar component
+
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+
+const App = () => {
+  return (
+     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+<h1> TEST</h1>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
